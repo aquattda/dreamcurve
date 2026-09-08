4521b8bf2ecc0d2e62b5b5e4fe1b8d9489480852
python <aquattda@gmail.com>
2026-09-08 08:24:10 +0700

    fix styles [vite.config.ts]

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], build: { sourcemap: true } });
+export default defineConfig({ plugins: [react()], server: { hmr: { port: Number(process.env.VITE_HMR_PORT || 24678) } }, build: { sourcemap: true } });
